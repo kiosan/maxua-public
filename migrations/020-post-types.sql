fcaefbe4e60a9adb13617d62256c9a7c50c88028
Max Ischenko <ischenko@gmail.com>
2025-05-11 17:04:03 +0300

    fixed post type

--- migrations/020-post-types.sql
+++ migrations/020-post-types.sql
@@ -1,9 +1,9 @@
 -- migrations/020-post-types.sql
--- Add support for different post types (regular, quote, link)
+-- Add support for different post types (text, quote, link)
 
 -- Add type and metadata columns to posts table
 ALTER TABLE posts 
-ADD COLUMN type VARCHAR(20) DEFAULT 'regular',
+ADD COLUMN type VARCHAR(20) DEFAULT 'text',
 ADD COLUMN metadata JSONB DEFAULT '{}';
 
 -- Create an index for faster type-based queries
@@ -11,8 +11,8 @@ CREATE INDEX idx_posts_type ON posts(type);
 
 -- Add constraint to ensure type is one of the allowed values
 ALTER TABLE posts 
-ADD CONSTRAINT posts_type_check CHECK (type IN ('regular', 'quote', 'link'));
+ADD CONSTRAINT posts_type_check CHECK (type IN ('text', 'quote', 'link'));
 
 -- Add comment for documentation
-COMMENT ON COLUMN posts.type IS 'Type of post: regular, quote, or link';
+COMMENT ON COLUMN posts.type IS 'Type of post: text, quote, or link';
 COMMENT ON COLUMN posts.metadata IS 'Type-specific metadata stored as JSON';
